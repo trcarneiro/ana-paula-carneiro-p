58ffdafcbefd24d8b36ca95b1ad6ab2f0c20cdda
Thiago Carneiro <trcampos@gmail.com>
2025-11-20 05:04:45 +0000

    Generated by Spark: Fix all reported errors.

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { ErrorBoundary } from "react-error-boundary";
 import "@github/spark/spark"
@@ -10,7 +11,9 @@ import "./styles/theme.css"
 import "./index.css"
 
 createRoot(document.getElementById('root')!).render(
-  <ErrorBoundary FallbackComponent={ErrorFallback}>
-    <App />
-   </ErrorBoundary>
+  <React.StrictMode>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
 )
